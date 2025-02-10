7239240a99703b91f21bc2823473e7cce4260a95
Hyacine Alnuma <halnuma@z1r10p1.42lyon.fr>
2025-02-10 15:25:18 +0100

    improved libft

--- libft/libc/src/ft_putstr_fd.c
+++ libft/libc/src/ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: halnuma <halnuma@student.42.fr>            +#+  +:+       +#+        */
+/*   By: halnuma <halnuma@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 12:25:51 by halnuma           #+#    #+#             */
-/*   Updated: 2025/01/08 12:45:28 by halnuma          ###   ########.fr       */
+/*   Updated: 2025/02/10 15:19:54 by halnuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ void	ft_putstr_fd(char *s, int fd)
 	int	s_len;
 
 	s_len = ft_strlen(s);
-	while (*s)
-		write(fd, s, s_len);
+	write(fd, s, s_len);
 }
--- libft/libc/src/ft_strlen.c
+++ libft/libc/src/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: halnuma <halnuma@student.42.fr>            +#+  +:+       +#+        */
+/*   By: halnuma <halnuma@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:59:21 by halnuma           #+#    #+#             */
-/*   Updated: 2024/11/07 16:30:18 by halnuma          ###   ########.fr       */
+/*   Updated: 2025/02/10 15:20:40 by halnuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,30 @@ size_t	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
+}
+
+size_t	ft_tablen(char **s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	count_char(char *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
 }
